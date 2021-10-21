562d69730d7d2abd19494d6b1e5fb26c87f0e17a
sf <90348284+exterror@users.noreply.github.com>
2021-10-22 02:07:55 +0300

    Initial commit

---
+++ script_.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let buttons = document.querySelectorAll(".button");
+    let output = document.querySelector(".display");
+    let current = 0;
+    let action = "";
+    let actions = {
+		"%": " % ",
+        "÷": " / ",
+        "X": " * ",
+        "−": " - ",
+        "+": " + "
+    };
+
+    for (let i in buttons) {
+        buttons[i].onclick = function (e) {
+            let input = e.target.innerText;
+            let num = parseInt(input);
+            if (isNaN(num)) {
+                if (input === "AC") {
+                    console.log("clear");
+                    action = null;
+                    current = 0;
+                    output.innerText = 0;
+                } else {
+                    if (action && action !== "=") {
+                        let calculation = current + actions[action] + output.innerText;
+                        console.log("calculate", calculation);
+                        output.innerText = eval(calculation);
+                    }
+                    current = parseInt(output.innerText);
+                    action = input;
+                }
+            } else {
+                if (current === parseInt(output.innerText)) {
+                    output.innerText = num;
+                } else {
+                    output.innerText += num;
+                }
+            }
+        };
+    }
+});
